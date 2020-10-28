7f93e8967ba06df62ae95d82828af0eacc871e4a
qiesi-1 <2516996736@qq.com>
2020-10-28 11:46:01 +0800

    区块创建

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -17,36 +17,68 @@ type BlockChain struct {
 //创建一条区块链
 
 func NewBlockChian() BlockChain {
-	//创世区块
-	genesis := CreateGenesisBlock()
+	var bc BlockChain
+	//1.先打开文件
 	db,err :=bolt.Open(BLOCKCHAIN,0600,nil)
-	if err!=nil {
-		panic(err.Error())
-	}
-
-	bc := BlockChain{
-		LastHash: genesis.Hash,
-		BoltDb:   db,
-	}
-	//把创世区块保存到数据库文件中
+	//2.查看chain。db文件
 	db.Update(func(tx *bolt.Tx) error {
-		bucket,err :=tx.CreateBucket([]byte(BLOCK_NAME))
-		if err!=nil{
-			panic(err.Error())
+		bucket :=tx.Bucket([]byte(BLOCK_NAME))//假设有桶
+		if bucket == nil{//没有桶，新建
+			bucket,err = tx.CreateBucket([]byte(BLOCK_NAME))
+			if err !=nil {
+				panic(err.Error())
+			}
 		}
-		//序列化
-		genesisBytes := genesis.Serialize()
-		//把创世区块储存到桶中
-		bucket.Put(genesis.Hash,genesisBytes)
-		//更新最新区块的hash值记录
-		bucket.Put([]byte(LAST_HASH),genesis.Hash)
-
+		lastHash :=bucket.Get([]byte(LAST_HASH))
+		if len(lastHash) ==0 {//桶中没有lasthash记录，徐常见创世区块，保存
+			//创世区块
+			genesis := CreateGenesisBlock()
+			genesisBytes := genesis.Serialize()
+			//创世区块保存到boltdb中
+			bucket.Put(genesis.Hash,genesisBytes)
+			//更新指向最新区块的lasthash值
+			bucket.Put([]byte(LAST_HASH),genesis.Hash)
+			bc = BlockChain{
+				LastHash: genesis.Hash,
+				BoltDb:   db,
+			}
+		}else {
+			lastHash1 := bucket.Get([]byte(LAST_HASH))
+			bc = BlockChain{
+				LastHash: lastHash1,
+				BoltDb:   db,
+			}
+		}
 
 		return nil
 	})
+	//if err!=nil {
+	//	panic(err.Error())
+	//}
+	//
+	//
+	////把创世区块保存到数据库文件中
+	//db.Update(func(tx *bolt.Tx) error {
+	//	bucket,err :=tx.CreateBucket([]byte(BLOCK_NAME))
+	//	if err!=nil{
+	//		panic(err.Error())
+	//	}
+	//	fmt.Println(bucket)
+	//	/*
+	//	//序列化
+	//	genesisBytes := genesis.Serialize()
+	//	//把创世区块储存到桶中
+	//	bucket.Put(genesis.Hash,genesisBytes)
+	//	//更新最新区块的hash值记录
+	//	bucket.Put([]byte(LAST_HASH),genesis.Hash)
+	//	*/
+	//
+	//	return nil
+	//})
 	return bc
 }
-//保存数据到区块链中： 先生成一个新区块，然后将新区块添加到区块链中
+
+//保存数据到区块链中；先生成一个新区块，然后将新区块添加到区块链中
 func(bc BlockChain ) SaveData(data []byte) {
 	//1.从文件读取到最新区块
 	db := bc.BoltDb
@@ -56,16 +88,26 @@ func(bc BlockChain ) SaveData(data []byte) {
 		if bucket != nil {
 			panic("读取区块链数据失败")
 		}
-		lastHash := bucket.Get([]byte(LAST_HASH))
-		lastBlockBytes := bucket.Get(lastHash)
+		//lastHash := bucket.Get([]byte(LAST_HASH))
+		//lastBlockBytes := bucket.Get(lastHash) 后面已经赋值，代码变更
+		lastBlockBytes := bucket.Get(bc.LastHash)
 		//反序列化
 		lastBlock,_ =DeSerialize(lastBlockBytes)
 		return nil
 	})
 	//新建一个区块
-	//newblock := NewBlock(lastBlock.Height+1,lastBlock.Hash,data)
+	newblock := NewBlock(lastBlock.Height+1,lastBlock.Hash,data)
 	//将新区块存到文件中
+	db.Update(func(tx *bolt.Tx) error {
+		bucket :=tx.Bucket([]byte(BLOCK_NAME))
+		//把新创建的区块存入到boltdb中
+		bucket.Put(newblock.Hash,newblock.Serialize())
+		//更新lasthash对应的值，更新为最新存储的区块的hash值
+		bucket.Put([]byte(LAST_HASH),newblock.Hash)
+		bc.LastHash =newblock.Hash//将区块链实例的lasthash值更新为最新区块的hash
 
+		return nil
+	})
 
 
 
--- main.go
+++ main.go
@@ -9,32 +9,11 @@ import (
 )
 
 func main() {
+	//1.创世区块
 
-	block0 := blockchain.CreateGenesisBlock()//创建创世区块
-	block1 := blockchain.NewBlock(block0.Height+1,block0.Hash,[]byte(""))
-	fmt.Printf("block0的哈希:%x\n",block0.Hash)
-	fmt.Printf("block1的prevhash:%x\n",block1.PrevHash)
-
-	//序列化
-	//将数据从内存中转化为可以持续存储在硬盘或在网络上传输的形式
-	/*
-	*	blockJson,_ := json.Marshal(block0)
-	*	fmt.Println("json序列化后的block：",string(blockJson))
-	*	blockXml,_ := xml.Marshal(block0)
-	*	fmt.Println("json序列化后的block：",string(blockXml))
-	*/
-	block0Bytes := block0.Serialize()
-	fmt.Println("创世区块gob序列化后",block0Bytes)
-	deBlock0 , err := blockchain.DeSerialize(block0Bytes)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println("反序列化的区块高度为",deBlock0.Height)
-	fmt.Printf("反序列化的区块hash为%x\n",deBlock0.Hash)
-
+	bc := blockchain.NewBlockChian()
+	fmt.Printf("创世区块的hash:%x\n",bc.LastHash)
 	return
-
 	//链接数据库
 	db_mysql.Connect()
 	//静态资源文件路径映射
